8f6ab266e1143f027f06272f992681dacf794e4f
Sharron <sking171@ford.com>
2022-03-25 15:29:17 -0500

    Edit js

--- exercise.js
+++ exercise.js
@@ -1,3 +1,18 @@
+function candySalesToObject() {
+    return candySales.sales.map(sale => sale.item)
+}
+console.log(candySalesToObject());
+
+function salesDayToObject() {
+    return candySales.date.find(candySales.date === "2015-01-07");
+}
+console.log(salesDayToObject());
+
+function allSalesToArray() {
+    return candySales.date.filter(sale => sale.date);
+}
+console.log(allSalesToArray());
+
 module.exports = {
     candySalesToObject,
     salesDayToObject,
